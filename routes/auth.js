e030ef86b3ce9a77abf06488d0c2878573f3599b
NOOBLAKHINA <87848999+NOOBLAKHINA@users.noreply.github.com>
2021-11-11 13:21:25 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const User = require('../models/User')
+const express = require("express")
+const router = express.Router()
+const { login, register } = require("../controllers/auth")
+router.post("/register", register)
+router.post("/login", login)
+module.exports = router
